6095392ecf0cb4b1853dcec3f09ada4acdfaa1d2
Malice0 <Amalicia29@gmail.com>
2023-02-15 09:50:09 -0300

    First part of router

---
+++ src/Pages/Housing/index.jsx
@@ -0,0 +1,9 @@
+function Housing() {
+    return (
+        <div>
+            <h1>Voici les Housing</h1>
+        </div>
+    )
+}
+
+export default Housing
--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './Styles/normalize.css'
 import './index.css'
 import Home from './Pages/Home/home'
+import About from './Pages/About/index'
+import Housing from './Pages/Housing/index'
 import Header from './Components/header/Hearder'
 import Footer from './Components/header/Footer/Footer'
-import './Styles/normalize.css'
+import Error from './Components/Error/index'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />,
+        errorElement: <Error />,
+        children: [
+            {
+                path: 'about',
+                element: <About />,
+            },
+            {
+                path: 'housing',
+                element: <Housing />,
+            },
+        ],
+    },
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
         <Header />
-        <Home />
+        <RouterProvider router={router} />
         <Footer />
     </React.StrictMode>
 )
