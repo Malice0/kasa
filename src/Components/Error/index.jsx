64667530a14b4452361824c4e306262d447b3d49
Malice0 <Amalicia29@gmail.com>
2023-02-20 14:26:32 -0300

    Error component

--- src/Components/Error/index.jsx
+++ src/Components/Error/index.jsx
@@ -1,11 +1,12 @@
 import '../../Styles/Error.css'
+import { Link } from 'react-router-dom'
 
 function Error() {
     return (
         <div className="error">
             <h1>404</h1>
             <h2>Oups! La page que vous demandez n'existe pas.</h2>
-            <p>Retourner sur la page d’accueil</p>
+            <Link to='/'>Retourner sur la page d’accueil</Link>
         </div>
     )
 }
