94bbdce322bc8af9dcbf2cebe062652363ea52b7
Malice0 <Amalicia29@gmail.com>
2023-02-22 16:41:05 -0300

    housing en cours

---
+++ src/Components/Rating.jsx
@@ -0,0 +1,11 @@
+import pinkstart from '../assets/pinkStart.svg'
+import greystart from '../assets/greyStart.svg'
+
+const Rating = ({ rating }) => {
+    //Construction d'un Array de 5 éléments qu'on rempli avec les étoiles
+    const rate = []
+
+    // rate = 
+}
+
+export default Rating
